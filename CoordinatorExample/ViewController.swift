c90ad001211f92f87ba240ff56a63ecf50e2e864
Apinun <apinun@Apinuns-MacBook-Pro-2.local>
2023-07-04 14:45:05 +0700

    feat: first install

---
+++ CoordinatorExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoordinatorExample
+//
+//  Created by Apinun on 4/7/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
